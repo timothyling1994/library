2eb864fb81a35a741aa926fb0776b90416e59222
Timothy Ling <timothyling1994@gmail.com>
2020-12-10 21:05:25 -0800

    Add more functionality and styling

--- prototype.js
+++ prototype.js
@@ -7,36 +7,94 @@ class Book {
     this.author = author
     this.numPages = numPages
     this.hasRead = hasRead
-    this.id
+    this.id = id
   }
 
 }
 
 function updateDisplay(index)
 {
-  //add index as data attribute to div 
+  const newDiv = document.createElement("div");
+  const newContent = document.createTextNode(myLibrary[index].name);
+  newDiv.appendChild(newContent);
+  newDiv.classList.add("book");
+  newDiv.setAttribute('data-attribute',index);
 
+  const booklist = document.querySelector(".booklist");
+  booklist.appendChild(newDiv,booklist);
+  //add index as data attribute to div 
+  
 }
 
 function addBookToLibrary()
 {
+  let title = document.querySelector("#title-input").value;
+  let author = document.querySelector("#author-input").value;
+  let numPages = document.querySelector("#numPages-input").value;
+  //let numPages = document.querySelector("#numPages-input").value;
+  let hasRead = false;
+
+  if(document.getElementById('hasRead').checked)
+  {
+    hasRead=true;
+  }
+
+
   let index = myLibrary.length;
-  let bookObj = new Book("Hard-Boiled","Murakami",405,true,index);
-  //let bookObj2 = new Book("Norwegian Wood","Murakami",200,true);
+  let bookObj = new Book(title,author,numPages,hasRead,index);
+  
   myLibrary.push(bookObj);
+
+  let modal = document.getElementById("modalWindow");
+  modal.style.display = "none";
+
   updateDisplay(index);
-  //myLibrary.push(bookObj2);
-  //console.log(myLibrary);
 }
 
-function enterBookDetails()
+
+function editBookDetails()
+{
+
+  //will only allow edit button to appear on already added books
+  //will use data-attribute of book div to access mylibrary array and edit book object
+}
+
+function removeBook()
 {
 
 }
 
 function theDomHasLoaded(e) {
-  const add_button = document.querySelector("#add-book");
-  add_button.addEventListener("click",enterBookDetails)
+
+  const edit_button = document.querySelector("#edit-book");
+  edit_button.addEventListener("click",editBookDetails)
+
+  const remove_button = document.querySelector("#remove-book");
+  remove_button.addEventListener("click",removeBook)
+
+  let modal = document.getElementById("modalWindow");
+  // Get the button that opens the modal
+  let btn = document.getElementById("add-book");
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  const submit_book = document.querySelector("#submit");
+  submit_book.addEventListener("click",addBookToLibrary)
 }
 
 document.addEventListener("DOMContentLoaded",theDomHasLoaded,false);
