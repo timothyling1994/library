fafc2cadc14fe4ed7679f98f6c24c279f15c3e65
Timothy Ling <timothyling1994@gmail.com>
2020-12-15 22:29:35 -0800

    Flesh out functionality for storing, editing, and deleting book details

--- prototype.js
+++ prototype.js
@@ -31,11 +31,19 @@ function updateDisplay(index)
   newDiv.setAttribute('data-attribute',index);
 
   const editButton = document.createElement("button");
-  const newContent = document.createTextNode(prop + ": " + book[prop]);
+  const editContent = document.createTextNode("Edit");
+
+  editButton.appendChild(editContent);
+  editButton.addEventListener("click",editBook)
 
   const deleteButton = document.createElement("button");
+  const deleteContent = document.createTextNode("Delete");
 
+  deleteButton.appendChild(deleteContent);
+  deleteButton.addEventListener("click",removeBook)
 
+  newDiv.appendChild(editButton);
+  newDiv.appendChild(deleteButton);
 
 
   const booklist = document.querySelector(".booklist");
@@ -67,28 +75,118 @@ function addBookToLibrary()
   modal.style.display = "none";
 
   updateDisplay(index);
+  
+  title.value="";
+  author.value="";
+  numPages.value="";
 }
 
 
-function editBookDetails()
+function editBook()
 {
 
-  //will only allow edit button to appear on already added books
-  //will use data-attribute of book div to access mylibrary array and edit book object
+  let edit_title= document.getElementById("edit-title-input");
+  let edit_author= document.getElementById("edit-author-input")
+  let edit_numPages= document.getElementById("edit-numPages-input")
+  let editHasRead = document.getElementById("edit-hasRead");
+  let editHasNotRead = document.getElementById("edit-hasNotRead");
+  let editModal = document.getElementById("editModalWindow");
+
+  editModal.style.display = "block";
+
+  let span = document.getElementsByClassName("close")[1];
+
+  span.onclick = function() {
+    editModal.style.display = "none";
+    edit_title.value="";
+    edit_author.value="";
+    edit_numPages.value="";
+
+
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == editModal) {
+      editModal.style.display = "none";
+      edit_title.value="";
+      edit_author.value="";
+      edit_numPages.value="";
+    }
+  }
+
+  let editDiv = this.parentElement;
+  let editIndex = editDiv.getAttribute("data-attribute");
+
+  let name_placeholder = myLibrary[editIndex].name;
+  let author_placeholder = myLibrary[editIndex].author;
+  let numPages_placeholder = myLibrary[editIndex].numPages;
+  let hasRead_placeholder = myLibrary[editIndex].hasRead;
+
+
+  edit_title.setAttribute("placeholder",name_placeholder); 
+  edit_author.setAttribute("placeholder",author_placeholder); 
+  edit_numPages.setAttribute("placeholder",numPages_placeholder);
+
+  if(hasRead_placeholder==true)
+  {
+   editHasRead.checked=true;
+  }
+  else
+  {
+    editHasNotRead.checked=true;
+  }
+
+  let submit_edits = document.getElementById("submit-edits");
+  
+  submit_edits.onclick = function(){
+    myLibrary[editIndex].name = edit_title.value;
+    myLibrary[editIndex].author = edit_author.value;
+    myLibrary[editIndex].numPages = edit_numPages.value;
+    if(editHasRead.checked)
+    {
+      myLibrary[editIndex].hasRead = true;
+    }
+    else
+    {
+      myLibrary[editIndex].hasRead = false; 
+    }
+
+    editDiv.childNodes[0].nodeValue = "name: " + myLibrary[editIndex].name;
+    editDiv.childNodes[2].nodeValue = "author: " + myLibrary[editIndex].author;
+    editDiv.childNodes[4].nodeValue = "numPages: " + myLibrary[editIndex].numPages;
+    editDiv.childNodes[6].nodeValue = "hasRead: " + myLibrary[editIndex].hasRead;
+
+    editModal.style.display = "none";
+    edit_title.value="";
+    edit_author.value="";
+    edit_numPages.value="";    
+  }
+
 }
 
 function removeBook()
 {
 
+  let indexToRemove = this.parentElement.getAttribute("data-attribute");
+  this.parentElement.remove();
+  myLibrary.splice(indexToRemove,1);
+
+  let bookNodeList = document.querySelectorAll(".book");
+
+  for (let i = 0;i<myLibrary.length;i++)
+  {
+    const book = myLibrary[i];
+    book.id = i;
+    
+    bookNodeList[i].setAttribute("id",i);
+    bookNodeList[i].setAttribute("data-attribute",i);
+
+    bookNodeList[i].childNodes[8].nodeValue = "id: " + i;
+
+  }
 }
 
 function theDomHasLoaded(e) {
-
-  const edit_button = document.querySelector("#edit-book");
-  edit_button.addEventListener("click",editBookDetails)
-
-  const remove_button = document.querySelector("#remove-book");
-  remove_button.addEventListener("click",removeBook)
 
   let modal = document.getElementById("modalWindow");
   // Get the button that opens the modal
